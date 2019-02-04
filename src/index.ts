3892d4266686a9457f6c9044650763b0a2e3cccf
tsurdilo <tsurdilo@redhat.com>
2019-02-04 15:13:51 -0500

    small update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import BpmnModdle from 'bpmn-moddle';
 import * as jbpmModdleJSON from './resources/jbpm.json';
 
-const fump = new BpmnModdle({ camunda: jbpmModdleJSON });
+const fump = new BpmnModdle({ jbpm: jbpmModdleJSON });
 
 export default fump;
